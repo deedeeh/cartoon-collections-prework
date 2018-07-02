ca65c0079c4c3481d731e70fc81664bb5f9522f6
Dina Hafez <dinahafez.8@gmail.com>
2018-07-02 08:43:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,76 +1,16 @@
-def roll_call_dwarves(dwarves)
+def roll_call_dwarves# code an argument here
   # Your code here
-  dwarves.each_with_index do |dwarves, i|
-    puts "#{i+1} #{dwarves}"
-  end
 end
 
-def summon_captain_planet(array)
+def summon_captain_planet# code an argument here
   # Your code here
-  array.collect {|item| item.capitalize + "!"}
 end
 
-def long_planeteer_calls(array)
+def long_planeteer_calls# code an argument here
   # Your code here
-  array.map do |item|
-    return item.length > 4 ? true : false
-  end
 end
 
-def find_the_cheese(array)
+def find_the_cheese# code an argument here
   # the array below is here to help
-  array.find do |item|
-    item.include?(array)
-  end
-end
-
-def roll_call_dwarves(dwarves)
-  # Your code here
-  dwarves.each_with_index do |dwarves, i|
-    puts "#{i+1} #{dwarves}"
-  end
-end
-
-def summon_captain_planet(array)
-  # Your code here
-  array.collect {|item| item.capitalize + "!"}
-end
-
-def long_planeteer_calls(array)
-  # Your code here
-  array.map do |item|
-    return item.length > 4 ? true : false
-  end
-end
-
-def find_the_cheese(array)
-  # the array below is here to help
-  array.find do |item|
-    item.include?(array)
-  end
-end
-def roll_call_dwarves(dwarves)
-  # Your code here
-  dwarves.each_with_index do |dwarves, i|
-    puts "#{i+1} #{dwarves}"
-  end
-end
-
-def summon_captain_planet(array)
-  # Your code here
-  array.collect {|item| item.capitalize + "!"}
-end
-
-def long_planeteer_calls(array)
-  # Your code here
-  array.map do |item|
-    return item.length > 4 ? true : false
-  end
-end
-
-def find_the_cheese(array)
-  # the array below is here to help
-  array.find do |item|
-    item.include?(array)
-  end
+  cheese_types = ["cheddar", "gouda", "camembert"]
 end
