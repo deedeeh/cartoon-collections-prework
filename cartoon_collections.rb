0015eb40f9163b8c2512fd01a86f97a4b3d86821
Dina Hafez <dinahafez.8@gmail.com>
2018-07-02 08:44:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.collect {|item| item.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
