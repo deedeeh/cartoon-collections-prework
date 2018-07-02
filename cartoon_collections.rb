ad6139b9bbab4b219284785246b0e2f679436fd5
Dina Hafez <dinahafez.8@gmail.com>
2018-07-02 08:42:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,3 +23,29 @@ def find_the_cheese(array)
     item.include?(array)
   end
 end
+
+def roll_call_dwarves(dwarves)
+  # Your code here
+  dwarves.each_with_index do |dwarves, i|
+    puts "#{i+1} #{dwarves}"
+  end
+end
+
+def summon_captain_planet(array)
+  # Your code here
+  array.collect {|item| item.capitalize + "!"}
+end
+
+def long_planeteer_calls(array)
+  # Your code here
+  array.map do |item|
+    return item.length > 4 ? true : false
+  end
+end
+
+def find_the_cheese(array)
+  # the array below is here to help
+  array.find do |item|
+    item.include?(array)
+  end
+end
